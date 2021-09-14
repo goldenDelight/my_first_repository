1721698f231c694984c4f22b110d5ea36325fddc
Ben <bencmorris@gmail.com>
2021-09-14 09:41:00 -0400

    fixed use of double/single quotation marks

--- quest.py
+++ quest.py
@@ -7,7 +7,8 @@ from selenium.common.exceptions import (
     TimeoutException, NoSuchElementException)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located as ec_visible
+from selenium.webdriver.support.expected_conditions import \
+    visibility_of_element_located as ec_visible
 from selenium.webdriver.support.wait import WebDriverWait
 
 import nav
@@ -29,13 +30,14 @@ def initialize_vars(driver) -> None:
 
     web_driver.print_temp("initializing grind vars")
 
-    WebDriverWait(driver, 5).until(ec_visible((By.ID, "canvas")))
+    WebDriverWait(driver, 5).until(ec_visible((By.ID, 'canvas')))
 
     canvas = driver.execute_script("return document.querySelector('#canvas');")
-    stam_bar = driver.execute_script("return document.querySelector('#stam_gage_num');")
+    stam_bar = driver.execute_script(
+        "return document.querySelector('#stam_gage_num');")
 
     if stam_bar is not None:
-        pre_click_stam = int(stam_bar.text.split('/')[0])
+        pre_click_stam = int(stam_bar.text.split("/")[0])
         current_stam = 0
 
 
@@ -43,25 +45,27 @@ def grind(driver, slayer_event=False):
     global canvas, loading_box, stam_bar, pre_click_stam, current_stam
 
     try:
-        loading_box = driver.find_element_by_id("loadingbox")
+        loading_box = driver.find_element_by_id('loadingbox')
         WebDriverWait(driver, 10).until(ec.staleness_of(loading_box))
-    except NoSuchElementException: pass
+    except NoSuchElementException:
+        pass
     except TimeoutException:
         WebDriverWait(driver, 10).until(ec.staleness_of(loading_box))
 
-    WebDriverWait(driver, 5).until(ec_visible((By.ID, "page_title_text")))
+    WebDriverWait(driver, 5).until(ec_visible((By.ID, 'page_title_text')))
     initialize_vars(driver)
 
     while pre_click_stam >= current_stam:
         try:
-            pre_click_stam = int(stam_bar.text.split('/')[0])
+            pre_click_stam = int(stam_bar.text.split("/")[0])
             click_cycle()
         except TimeoutException: break
         except StaleElementReferenceException:
             initialize_vars(driver)
         except AttributeError:
             try:
-                canvas = driver.execute_script("return document.querySelector('#canvas');")
+                canvas = driver.execute_script(
+                    "return document.querySelector('#canvas');")
                 canvas.click()
             except StaleElementReferenceException: break
             except AttributeError: break
