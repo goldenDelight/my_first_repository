dc72db9eed671dc0e15eae19ef1274e9622c189d
Ben <bencmorris@gmail.com>
2021-09-28 10:47:19 -0400

    moved handler methods to utilities

--- utilities.py
+++ utilities.py
@@ -179,3 +179,48 @@ def return_to_event(driver):
     goto_event = driver.execute_script(
         "return document.querySelector('#event_menu_2 > div:nth-child(3) > a');")
     driver.execute_script("arguments[0].click();", goto_event)
+
+
+def restore_stam(self):
+    use_small_stams = self.execute_script(
+        'return document.querySelectorAll(\'.decision_button_column_2')
+    self.execute_script("arguments[0].click();", use_small_stams[0])
+
+    WebDriverWait(self, 3).until(ec.staleness_of(use_small_stams))
+
+    confirm_use = self.find_element_by_css_selector(
+        '#main_frame_item > div:nth-child(5) > a:nth-child(1)')
+    self.execute_script("arguments[0].click();", confirm_use)
+
+    WebDriverWait(self, 3).until(ec.staleness_of(confirm_use))
+
+    return_to_event = self.find_element_by_css_selector(
+        '#main_frame_item > div:nth-child(5) > a:nth-child(1)')
+    self.execute_script("arguments[0].click();", return_to_event)
+
+    WebDriverWait(self, 3).until(ec.staleness_of(return_to_event))
+
+#     locate, click last button to return to grind_routine
+    all_anchors = self.execute_script("return document.querySelectorAll('a');")
+    for a in all_anchors:
+        href = a.get_attribute('href')
+        if '/tower/tower_start' in href:
+            self.execute_script("arguments[0].click();", a)
+
+
+def card_sales(driver):
+    driver.execute_script("return document.querySelector('#numbner_of_card');")
+    bulk = driver.execute_script("return document.querySelector('#button_bulk');")
+    driver.execute_script("arguments[0].click();", bulk)
+
+
+def screen_cards(driver):
+    cards_to_sell = driver.execute_script("return document.querySelectorAll('div[id^=showcase_frame]');")
+
+    for card in cards_to_sell:
+        info = driver.execute_script("return arguments[0].querySelectorAll('*');", card)
+        for i in info:
+            if i.get_attribute('src') == 'https://cf.tna.dmmgames.com/img/common/card/S/C00040b.' \
+                                         '73fcabcb223e0a96e48159015766757a.png':
+                print(i.get_attribute('id'))
+                print(cards_to_sell.index(card))
