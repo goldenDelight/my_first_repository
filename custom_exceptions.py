e20b47941c7a8977302c3796a1cf5c614e3812ab
Ben <bencmorris@gmail.com>
2022-06-11 12:08:05 -0400

    Added initialization to the custom errors, henceforth enabling them to exist.

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -1,28 +1,46 @@
 class NoProgressException(Exception):
     """Raised when clicking canvas no longer reduces stamina"""
-    print("no progress exception raised")
+
+    def __init__(self):
+        super().__init__()
+        print("no progress exception raised")
 
 
 class MaxCardLimitException(Exception):
     """Raised when card-limit-reached redirect is confirmed"""
-    print("max card limit exception raised")
+
+    def __init__(self):
+        super().__init__()
+        print("max card limit exception raised")
 
 
 class NotEnoughStaminaException(Exception):
     """Raised when presence of 'not enough stamina' pop-up is confirmed"""
-    print("not enough stamina exception raised")
+
+    def __init__(self):
+        super().__init__()
+        print("not enough stamina exception raised")
 
 
 class RequestError0(Exception):
     """Raised when 'request error(0)' text in game frame"""
-    print("request error detected")
+
+    def __init__(self):
+        super().__init__()
+        print("request error detected")
 
 
 class WtfException(Exception):
     """Raised when wtf?!"""
-    print("wtf")
+
+    def __init__(self):
+        super().__init__()
+        print("wtf")
 
 
 class ShopBreakException(Exception):
     """Raised when breaking the shackles of capitalism"""
-    print("well done comrade")
+
+    def __init__(self):
+        super().__init__()
+        print("well done comrade")
