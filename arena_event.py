49319b1108640b7698e1f2595a66d121f3f0c9fe
Ben <bencmorris@gmail.com>
2022-07-07 10:00:27 -0400

    Old custom methods that have been found to no longer be referenced anywhere.

--- arena_event.py
+++ arena_event.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
+import nav
 import utilities
 
 # TODO:
