859ee34def90c0e5e70bcfc2763f5bd319dda852
Ben <bencmorris@gmail.com>
2022-07-24 16:01:42 -0400

    arena event overhaul: more consistent element finding battle stats are now global variables. added event score breakpoint. pick_fight doesn't break on rank-up battles alpha implementation of deck change

--- arena_event.py
+++ arena_event.py
@@ -23,52 +23,65 @@ import utilities
 #  check/change decks based on fever/rank-up
 from custom_exceptions import ShopBreakException
 
+shit_list = {}
+event_points = 0
+fight_count = 0
+loss_count = 0
+op_name = None
+global pts
+
 
 def grind(driver):
 
-    if driver.switch_to.parent_frame():
-        print("switched to parent frame\r")
-    if WebDriverWait(driver, 10).until(
-            ec.frame_to_be_available_and_switch_to_it((By.ID, "game_frame"))):
-        print("switched to game frame\r")
+    if event_points > 1000000:
+        raise ShopBreakException
 
-    pick_fight(driver)
+    fever = False
 
+    driver.bot.refocus_frame()
 
-def pick_fight(driver):
-    opponent_frames_list = WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.ID, "productList")))
+    if '/arena/battle_index' not in driver.bot.page():
+        nav.arena(driver)
 
     time.sleep(1)
-    print(driver.execute_script("return document.querySelector('#fev_timer');").text)
+    print(driver.execute_script(
+        "return document.querySelector('#fev_timer');").text)
 
-    if driver.execute_script("return document.querySelector('#fev_timer');").text != "":
+    if driver.execute_script(
+            "return document.querySelector('#fev_timer');").text != "":
         print('fever time yes')
-        is_fever_time = True
+        fever = True
+
+    pick_fight(driver, fever)
+    skip_animation(driver)
+    get_battle_results(driver)
+    nav.arena(driver)
+
+
+def pick_fight(driver, fever):
+    global shit_list
+    global op_name
+
+    WebDriverWait(driver, 5).until(
+        ec.visibility_of_element_located((By.ID, "productList")))
+
+    for i in range(3):
+        driver.bot.refocus_frame()
+        products = driver.execute_script(
+            "return document.querySelector('#productList');")
+        frames = products.find_elements(By.TAG_NAME, 'a')
+        opponent = frames[i].text.split()
+
+        if (op_name := opponent[0]) not in shit_list:
+            if "Advancement" not in opponent:
+                driver.execute_script("arguments[0].click();", frames[i])
+                WebDriverWait(driver, 10).until(ec.staleness_of(frames[i]))
+                break
+
+    if fever:
+        weak_attack(driver)
     else:
-        is_fever_time = False
-        print('fever time no')
-
-    opponent_frame = opponent_frames_list.find_elements(By.TAG_NAME, 'a')[1]
-    # print(opponent_frame.text)
-    driver.execute_script("arguments[0].click();", opponent_frame)
-
-    try:
-        WebDriverWait(driver, 10).until(ec.staleness_of(opponent_frames_list))
-
-        if is_fever_time:
-            weak_attack(driver)
-        else:
-            normal_attack(driver)
-
-        skip_animation(driver)
-        get_battle_results(driver)
-    except TimeoutException:
-        utilities.print_temp("timed out :(")
-
-    finally:
-        import nav
-        nav.arena(driver)
+        normal_attack(driver)
 
 
 def weak_attack(driver):
@@ -122,62 +135,105 @@ def skip_animation(driver):
 
 
 def get_battle_results(driver):
-    op_name = -1
-    op_def = -1
-    me_atk = -1
-    battle_loss = -1
+    global event_points
+    global fight_count
+    global loss_count
+
+    driver.bot.refocus_frame()
+    op_def = 0
+    me_atk = 0
+    result = 'win'
+
+    fight_count += 1
 
     points_frame = driver.execute_script(
         "return document.querySelector('#scroll_content2');")
     divs = points_frame.find_elements(By.TAG_NAME, 'div')
 
     for div in divs:
-        driver.execute_script("arguments[0].scrollTop=arguments[1].offsetTop",
-                              points_frame, div)
+        driver.execute_script(
+            "arguments[0].scrollTop=arguments[1].offsetTop", points_frame, div)
+
         line = div.text
 
         if line.startswith("Total Arena pts "):
             pts = int((line.split()[3])[:-2])
-            total = int(driver.bot.arena_event_points)
-            driver.bot.arena_event_points = total + pts
-
-            count = int(driver.bot.arena_event_fight_count)
-            driver.bot.arena_event_fight_count = count + 1
+            event_points += pts
 
             if pts < 1000:
-                loss = driver.bot.arena_event_loss_count
-                driver.bot.arena_event_loss_count = loss + 1
-                battle_loss = True
+                result = "loss"
+                loss_count += 1
 
-                try:
-                    me_atk = driver.execute_script("return document.querySelector('#main_frame_battle > a:nth-child(8) > div > div:nth-child(2) > div.result_attack_frame_lose');")
-                    me_atk = int(me_atk.text)
-                    time.sleep(0.5)
+        try:
+            shadows = driver.execute_script(
+                "return document.querySelectorAll('.shadow');")
+            me_atk = int(shadows[0].text)
+            op_def = int(shadows[1].text)
+        except AttributeError:
+            pass
 
-                    op_def = driver.execute_script("return document.querySelector('#main_frame_battle > a:nth-child(8) > div > div:nth-child(2) > div.result_defence_frame_win');")
-                    op_def = int(op_def.text)
-                    time.sleep(0.5)
+    utilities.print_temp(f"Result: {result}\n\tpoints: {pts:,}", temp=False)
+    if result == 'loss':
+        shit_list[op_name] = op_def
+        print(f"\topponent: {op_name} (added to shit list)", flush=True)
+    else:
+        print(f"\topponent: {op_name}", flush=True)
 
-                    op_name = driver.execute_script("return document.querySelector('#main_frame_battle > a:nth-child(8) > div > div:nth-child(3)');")
-                    op_name = op_name.text
-                    time.sleep(0.5)
+    print(f"\twin/loss: {fight_count-loss_count}/{loss_count}", flush=True)
+    print(f"\tpoints per fight: {int(event_points/fight_count):,}", flush=True)
+    print(f"\tmy atk: {me_atk:,}", flush=True)
+    print(f"\top def: {op_def:,}", flush=True)
 
-                except AttributeError:
-                    pass
-            average = ((total+pts)/(count+1))
+    driver.bot.arena_event_points += pts
 
-            if battle_loss:
-                utilities.print_temp(f"my atk: {me_atk}", temp=False)
-                utilities.print_temp(f"op def: {op_def} ({op_name})", temp=False)
-                utilities.print_temp(f"arena_fight_count: {driver.bot.arena_event_fight_count}", temp=False)
-                utilities.print_temp(f"arena_loss_count: {driver.bot.arena_event_loss_count}", temp=False)
-                utilities.print_temp(f"pts/fight: {int(average)}\n\n", temp=False)
-                break
-            else:
-                # taba_bot.print_temp(f"win margin: {me_atk - op_def}", temp=False)
-                utilities.print_temp(f"arena_pts: {pts}", temp=False)
-                utilities.print_temp(f"arena_pts_total: {driver.bot.arena_event_points}", temp=False)
-                utilities.print_temp(f"arena_fight_count: {driver.bot.arena_event_fight_count}", temp=False)
-                utilities.print_temp(f"arena_loss_count: {driver.bot.arena_event_loss_count}", temp=False)
-                utilities.print_temp(f"pts/fight: {int(average)}\n\n", temp=False)
-                break
+
+def special_deck(driver):
+    driver.bot.refocus_frame()
+    child = driver.find_element(By.XPATH,
+                                '//*[@id="main_frame_battle"]/div[2]/img[5]')
+    return 'C00000a' in child.get_attribute('src')
+
+
+def deck_edit(driver):
+    deb = driver.bot.find_href("/deck/deck_index")
+    driver.execute_script("arguments[0].click();", deb)
+
+    while driver.bot.page() == '/arena/battle_index':
+        time.sleep(0.1)
+
+    print("page changed")
+
+
+def change_deck(driver):
+    driver.bot.refocus_frame()
+
+    try:
+        bdc = driver.find_element(By.ID, "button_deck_change_2")
+        print('deck 2 clickable')
+    except NoSuchElementException:
+        bdc = driver.find_element(By.ID, "button_deck_change_3")
+        print('deck 3 clickable')
+
+    driver.execute_script("arguments[0].click();", bdc)
+    WebDriverWait(driver, 10).until(ec.staleness_of(bdc))
+
+    while driver.bot.page() == '/deck/deck_index':
+        driver.execute_script("app.back();")
+
+
+# PRINTS ALL VISIBLE OPPONENT NAMES
+
+
+# iterate opponent frames
+# check the frame text to confirm not an advancement battle
+# get opponent name
+# click frame to fight that opponent
+
+
+# PRINTS FINAL ATTACK AND/OR DEF RESULTS
+# on page '/arena/user_result'
+# driver.switch_to.parent_frame()
+# WebDriverWait(driver, 10).until(
+#     ec.frame_to_be_available_and_switch_to_it((By.ID, "game_frame")))
+# shadows = driver.execute_script("return document.querySelectorAll('.shadow');")
+# print(shadows[1].text)
