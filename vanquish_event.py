c98fe5d233aff3027b6033f20a7c86a9d9b2ecaa
Ben <bencmorris@gmail.com>
2022-07-24 15:44:23 -0400

    event navigation moved from nav to event module

--- vanquish_event.py
+++ vanquish_event.py
@@ -2,6 +2,7 @@ import selenium.webdriver.support.expected_conditions as ec
 from selenium.common.exceptions import (JavascriptException,
                                         TimeoutException)
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 import logic
@@ -14,7 +15,7 @@ from custom_exceptions import \
     RequestError0, \
     ShopBreakException
 
-
+vanquish_points_goal = 200000000
 full_attack_AR = False
 current_points = 0
 
@@ -42,6 +43,16 @@ def nav_to_stage(driver):
         ref = driver.bot.find_href('hunt_start')
         driver.execute_script("arguments[0].click();", ref)
 
+
+# TODO: change click method to javascript click to avoid 'click intercepted'
+#  exceptions when a popup is displayed
+
+def run_event():
+    global current_points
+    global vanquish_points_goal
+
+    # return current_points < vanquish_points_goal
+    return True
 
 
 def grind(driver):
@@ -143,3 +154,45 @@ def check_for_boss(driver):
             AttributeError,
             JavascriptException):
         return False
+
+
+def battle_to_event_stage(driver):
+    global vanquish_points_goal
+    global current_points
+
+    WebDriverWait(driver, 5).until(ec.visibility_of_element_located(
+        (By.ID, 'hunt_result')))
+
+    current_points = int(driver.execute_script("return app.tower.data.extra_pt"))
+
+    if current_points is not None:
+        print(f"total event points: {current_points:,}\n")
+
+        if current_points > vanquish_points_goal:
+            raise ShopBreakException
+
+    result = driver.find_element(By.ID, 'hunt_result')
+    close = result.find_element(By.CLASS_NAME, 'closePopup')
+    driver.execute_script("arguments[0].click();", close)
+
+    WebDriverWait(driver, 5).until(ec.presence_of_all_elements_located(
+        (By.TAG_NAME, 'a')))
+
+    hunt = driver.execute_script(
+        "return document.querySelector('a[href*=hunt_start]');")
+
+    driver.execute_script("arguments[0].click();", hunt)
+    WebDriverWait(driver, 5).until(ec.staleness_of(hunt))
+
+
+def event_stage(driver):
+    ref = driver.bot.find_href('hunt_start')
+    driver.execute_script("arguments[0].click();", ref)
+    WebDriverWait(driver, 10).until(ec.staleness_of(ref))
+
+
+def event_page(driver):
+    ref = driver.bot.find_href('hunt_event_top')
+    driver.execute_script("arguments[0].click();", ref)
+    WebDriverWait(driver, 10).until(ec.staleness_of(ref))
+    return True
